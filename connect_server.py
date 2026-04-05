090d113f76caa741d2b0ee9ac611b2858db89d42
Ryan <164410629+Rdtcm@users.noreply.github.com>
2026-04-04 21:51:21 -0300

    Create connect_server.py

---
+++ connect_server.py
@@ -0,0 +1,22 @@
+import socket #importa modulo socket
+
+TCP_IP = '127.0.0.1' # endereço IP do servidor 
+TCP_PORTA = 10352      # porta disponibilizada pelo servidor
+TAMANHO_BUFFER = 1024
+
+MENSAGEM  = input("Digite sua mensagem para o servidor: ")
+
+# Criação de socket TCP do cliente
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+while True: 
+  # Conecta ao servidor em IP e porta especifica 
+  cliente.connect((TCP_IP, TCP_PORTA))
+  
+  # envia mensagem para servidor 
+  cliente.send(MENSAGEM.encode('UTF-8'))
+  
+  # recebe dados do servidor 
+  data, addr = cliente.recvfrom(1024)
+  
+  print ("received data:", data)
